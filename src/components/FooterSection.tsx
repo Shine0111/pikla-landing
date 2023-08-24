8c61d1d087095ce1d0b678d4925ae7d1f0cc16c5
Shine Ran <ranshine9@gmail.com>
2023-08-24 13:57:47 +0300

    Started footer section with layout

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -1,8 +1,26 @@
-import { Divider, HStack, Text, Stack, IconButton } from "@chakra-ui/react";
+import {
+  Divider,
+  HStack,
+  VStack,
+  Text,
+  Stack,
+  IconButton,
+  Image,
+  Heading,
+  List,
+  ListItem,
+} from "@chakra-ui/react";
 import { MdFacebook } from "react-icons/md";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { TiSocialLinkedinCircular } from "react-icons/ti";
 import { TiSocialInstagramCircular } from "react-icons/ti";
+
+import logo from "../assets/logo-long.png";
+import appStore from "../assets/app-store.webp";
+import googlePlay from "../assets/google-play.webp";
+import apkDownload from "../assets/apk-download.webp";
+import services from "../data/services";
+import { Link } from "react-router-dom";
 
 const FooterSection = () => {
   return (
@@ -13,7 +31,90 @@ const FooterSection = () => {
       className="innerWidth"
       justifyContent="space-between"
     >
-      <div>first part of footer</div>
+      <Stack
+        direction={{
+          base: "column",
+          sm: "column",
+          md: "column",
+          lg: "row",
+        }}
+        justifyContent="space-between"
+      >
+        <VStack>
+          <button className="footer-heading">
+            <Image src={logo} width="125px" />
+          </button>
+          <button>
+            <Image src={appStore} width="150px" />
+          </button>
+          <button>
+            <Image src={googlePlay} width="150px" />
+          </button>
+          <button>
+            <Image src={apkDownload} width="150px" />
+          </button>
+        </VStack>
+        <Stack
+          direction={{
+            base: "column",
+            sm: "column",
+            md: "column",
+            lg: "row",
+          }}
+          gap="2rem"
+        >
+          <VStack alignItems="flex-start">
+            <Heading className="footer-heading" fontSize="lg">
+              Services
+            </Heading>
+            <List>
+              {services.map((service) => (
+                <ListItem key={service.title}>
+                  <Link to={service.to}>{service.title}</Link>
+                </ListItem>
+              ))}
+            </List>
+          </VStack>
+
+          <VStack alignItems="flex-start">
+            <Heading className="footer-heading" fontSize="lg">
+              Join our team
+            </Heading>
+            <List>
+              <ListItem>
+                <Link to="about-us">About us</Link>
+              </ListItem>
+              <ListItem>
+                <Link to="engineering-at-pikla">Engineering at pikla</Link>
+              </ListItem>
+              <ListItem>
+                <Link to="/">Careers</Link>
+              </ListItem>
+              <ListItem>
+                <Link to="/">Explore open roles</Link>
+              </ListItem>
+              <ListItem>
+                <Link to="/">Why Pikla</Link>
+              </ListItem>
+            </List>
+          </VStack>
+
+          <VStack alignItems="flex-start">
+            <Heading className="footer-heading" fontSize="lg">
+              About us
+            </Heading>
+            <List>
+              <ListItem>
+                <Link to="our-social-impact">Our Social Impact</Link>
+              </ListItem>
+              <ListItem>
+                <Link to="/">Blog</Link>
+              </ListItem>
+            </List>
+          </VStack>
+        </Stack>
+        <Text>Lang</Text>
+      </Stack>
       <Divider />
       <Stack
         direction={{
--- src/data/services.ts
+++ src/data/services.ts
@@ -1,16 +1,30 @@
-
-
 interface Services {
-    title: string;
-    categories: string[];
-    background: string;
+  title: string;
+  categories: string[];
+  background: string;
+  to: string;
 }
 
 const services: Services[] = [
-    {title: 'Go', categories: ['taxi', 'suv'], background: 'blue'},
-    {title: 'Eat', categories: ['Livraison', 'Find'], background: 'purple'},
-    {title: 'Livraison', categories: ['Livraison'], background: 'indigo'},
-    {title: 'Medicament', categories: ['Medicament'], background: 'green'},
-]
+  { title: "Go", categories: ["taxi", "suv"], background: "blue", to: "/" },
+  {
+    title: "Eat",
+    categories: ["Livraison", "Find"],
+    background: "purple",
+    to: "/",
+  },
+  {
+    title: "Livraison",
+    categories: ["Livraison"],
+    background: "indigo",
+    to: "/",
+  },
+  {
+    title: "Medicament",
+    categories: ["Medicament"],
+    background: "green",
+    to: "/",
+  },
+];
 
 export default services;
