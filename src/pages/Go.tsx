84fb818f88a08bc588a40e1c75e2d69d1ec7b652
Shine Ran <ranshine9@gmail.com>
2023-09-04 16:01:46 +0300

    a description section to go page

--- src/pages/Go.tsx
+++ src/pages/Go.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "@chakra-ui/react";
+import { Heading, Stack, VStack, Text } from "@chakra-ui/react";
 import Hero from "../components/Hero";
 import CategoryLink from "../components/CategoryLink";
 import services from "../data/services";
@@ -10,8 +10,23 @@ const Go = () => {
       <Heading textAlign="center" color="var(--dark-blue)" fontSize="6xl">
         Go
       </Heading>
-
       <CategoryLink links={services[0].categories} horizontal={true} />
+      <Stack
+        direction={{
+          base: "column-reverse",
+          sm: "column-reverse",
+          md: "column-reverse",
+          lg: "row",
+        }}
+        className="innerWidth"
+      >
+        <VStack alignItems="flex-start">
+          <Heading fontSize="6xl" color="var(dark-blue)">
+            Rides
+          </Heading>
+          <Text>Order a ride with Pikla, and go further, go faster</Text>
+        </VStack>
+      </Stack>
     </div>
   );
 };
