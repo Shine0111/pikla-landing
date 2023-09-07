6a2b021067f8d47a0003f525f15f17442e917da9
Shine Ran <ranshine9@gmail.com>
2023-09-07 15:29:14 +0300

    render categories depends on clicked category

--- src/pages/Go.tsx
+++ src/pages/Go.tsx
@@ -16,6 +16,27 @@ const Go = () => {
   const handleLinkClick = (text: string) => {
     setClickedService(text);
   };
+  if (location.state)
+    return (
+      <div>
+        <PageTitle titleAddOn="Go" />
+        <Hero buttonText="Book your Ride" buttoned />
+        <Heading textAlign="center" color="var(--dark-blue)" fontSize="6xl">
+          Go
+        </Heading>
+        <CategoryLink
+          links={services[0].categories}
+          horizontal={true}
+          onClickCallback={handleLinkClick}
+          clickedService={location.state}
+        />
+        {location.state === "Rides" && <Rides />}
+        {location.state === "Taxi" && <Taxi />}
+        {location.state === "CarRental" && <CarRental />}
+        {location.state === "Taxi-moto" && <TaxiMoto />}
+        <ScrollRestoration />
+      </div>
+    );
   return (
     <div>
       <PageTitle titleAddOn="Go" />
@@ -23,7 +44,6 @@ const Go = () => {
       <Heading textAlign="center" color="var(--dark-blue)" fontSize="6xl">
         Go
       </Heading>
-      <p>Data : {location.state}</p>
       <CategoryLink
         links={services[0].categories}
         horizontal={true}
